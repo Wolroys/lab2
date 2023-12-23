953eb69d90e99e5123630e761748039a69ec3fb2
Wolroys <alekseypaslenov@gmail.com>
2023-12-23 23:45:07 +0300

    finished 1 task

--- src/main/java/org/wolroys/Main.java
+++ src/main/java/org/wolroys/Main.java
@@ -4,6 +4,32 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        //Task 1
 
+        String[] array = {
+                "dog",
+                "cat",
+                "bird",
+                "phone",
+                "dog",
+                "plane",
+                "spoon",
+                "car",
+                "cat",
+                "bird",
+                "god",
+                "dog",
+                "car",
+                "bicycle",
+                "cat",
+                "phone"
+        };
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : array)
+            map.put(word, map.getOrDefault(word, 0) + 1);
+
+        for (Map.Entry<String, Integer> entry : map.entrySet())
+            System.out.println(entry.getKey() + " - " + entry.getValue());
     }
 }
