ea644ff7723b500f0b3f9bcfd4146888ecf6206c
Wolroys <alekseypaslenov@gmail.com>
2023-12-24 00:09:50 +0300

    finished 2 task

--- src/main/java/org/wolroys/Main.java
+++ src/main/java/org/wolroys/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //Task 1
 
+        System.out.println("---- 1 Task ----");
         String[] array = {
                 "dog",
                 "cat",
@@ -25,11 +26,31 @@ public class Main {
                 "phone"
         };
 
+        List<String> uniqueWord = Arrays.stream(array).distinct().toList();
+
+        System.out.println("Список уникальных слов: ");
+        System.out.println(uniqueWord);
+
+        System.out.println("Количество вхождений каждого слова: ");
         Map<String, Integer> map = new HashMap<>();
         for (String word : array)
             map.put(word, map.getOrDefault(word, 0) + 1);
 
         for (Map.Entry<String, Integer> entry : map.entrySet())
             System.out.println(entry.getKey() + " - " + entry.getValue());
+
+
+        System.out.println("---- 2 Task ----");
+
+        PhoneDirectory.add("Paslenov", "8-800-555-35-35");
+        PhoneDirectory.add("Paslenov", "8-888-888-88-88");
+        PhoneDirectory.add("Petrov", "8-888-888-88-82");
+        PhoneDirectory.add("Ivanov", "8-488-858-88-82");
+        PhoneDirectory.add("Ivanov", "8-488-898-88-82");
+
+        System.out.println("Paslenov: " + PhoneDirectory.get("Paslenov"));
+        System.out.println("Petrov: " + PhoneDirectory.get("Petrov"));
+        System.out.println("Ivanov: " + PhoneDirectory.get("Ivanov"));
     }
+
 }
---
+++ src/main/java/org/wolroys/PhoneDirectory.java
@@ -0,0 +1,22 @@
+package org.wolroys;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class PhoneDirectory {
+    private static Map<String, Set<String>> list;
+
+    static {
+        list = new HashMap<>();
+    }
+
+    public static void add(String surname, String phoneNumber) {
+        list.computeIfAbsent(surname, key -> new HashSet<>()).add(phoneNumber);
+    }
+
+    public static Set<String> get(String surname) {
+        return list.getOrDefault(surname, new HashSet<>());
+    }
+}
